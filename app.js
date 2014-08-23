c1af6e6557a6132ded551c4df7e73e68f65a1142
drei01 <mcreidie@hotmail.com>
2014-08-23 09:34:48 +0100

    port over from main branch

--- app.js
+++ app.js
@@ -4,26 +4,36 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var coffeescript = require('connect-coffee-script');
+var connect = require('connect');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+app.set('partials', {body: 'body'});
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
+//compass
+app.use(require('node-compass')({
+    sass: 'styles',
+    css: 'styles'
+}));
+//coffeescript
+app.use(coffeescript({
+  src: path.join(__dirname, 'public'),
+  bare: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +66,7 @@ app.use(function(err, req, res, next) {
     });
 });
 
+
+app.listen(8000);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'BakeOff App | Discover recipes, challenge your friends',
+	url: req.protocol + '://' + req.get('host') + req.originalUrl,
+	og_title: 'BakeOff',
+	og_description: 'Discover recipes, challenge your friends',
+	og_image: req.protocol + '://' + req.get('host') + '/images/placeit_400.png',
+	
+    partials: {     
+      layout: 'layout'   
+    }       
+    });
 });
 
 module.exports = router;
